df4d2daaf42ad697d811e9cb93c34a86dc15fce2
wellyandriani <wellyandriani97@gmail.com>
2018-07-10 16:08:08 +0700

    Create some if else statement

---
+++ level8.js
@@ -0,0 +1,13 @@
+//level 8
+console.log("LEVEL 8");
+
+function test(y) {
+    if (y > 0) {
+      return "positive";
+    } else {
+      return "NOT positive";
+    }
+  }
+  
+  console.log(test(10));
+  
