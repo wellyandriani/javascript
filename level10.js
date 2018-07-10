ff1a670ae82404b629473fba5f2cf4d8a069a329
wellyandriani <wellyandriani97@gmail.com>
2018-07-10 16:41:18 +0700

    Use ternary operator ? : rather than if else statement

--- level10.js
+++ level10.js
@@ -4,11 +4,11 @@ console.log("LEVEL 10");
 console.log("Determine the age to drink alcohol");
 var age = 26;
 var canDrinkAlcohol = (age > 21) ? "True, over 21" : "False, under 21";
+console.log(canDrinkAlcohol);
 
 console.log("Amount of money to buy shoes")
 function buyshoes(price){
     return (price ? "Rp.500000" : "Rp.5000");
 }
-console.log(canDrinkAlcohol);
 console.log(buyshoes(true));
 console.log(buyshoes(false));
