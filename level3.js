8c75fa9774aae132b2e8a5e9d9d14baae60c5a6b
wellyandriani <wellyandriani97@gmail.com>
2018-07-10 14:37:25 +0700

    Add js level 3

---
+++ level3.js
@@ -0,0 +1,12 @@
+//level 3
+
+console.log("LEVEL 3");
+
+var readme = ["123", "true", "me"]
+var mean = ["false", "200000000000000", "indonesia"]
+
+console.log("readme:", readme);
+console.log("mean:", mean);
+
+var resultA = readme.push("e")
+var resultB = mean.pop()
