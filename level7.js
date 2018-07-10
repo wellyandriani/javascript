ab70e098ea3d30c15909b5680b58bd930ce68952
wellyandriani <wellyandriani97@gmail.com>
2018-07-10 15:46:31 +0700

    level 7

---
+++ level7.js
@@ -0,0 +1,14 @@
+//level 7
+console.log("LEVEL 7");
+
+let title = "Welly Andriani"
+
+function toTitleCase(srt) {
+    return str.replace(/(?:^|\s)\w/g, function (match.toUpperCase())
+    return match.toUpperCase();
+
+})
+}
+console.log(title.toLowerCase())
+console.log(title.toUpperCase())
+console.log(toTitleCase())
