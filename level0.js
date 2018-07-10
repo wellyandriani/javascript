4349703f1e891dd957c91adde331ea66dd23a900
wellyandriani <wellyandriani97@gmail.com>
2018-07-10 13:27:04 +0700

    add js level0

---
+++ level0.js
@@ -0,0 +1,10 @@
+//level 0
+console.log("LEVEL 0");
+
+let numberA = 10
+let numberB = 20
+const numberC = 30
+
+console.log("numberA:", numberA);
+console.log("numberB:", numberB);
+console.log("numberC:", numberC);
