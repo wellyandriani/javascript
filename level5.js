6352e3e1fc0305d35ba064c7d23ed0eaae65118e
wellyandriani <wellyandriani97@gmail.com>
2018-07-10 15:01:28 +0700

    add js level 5

---
+++ level5.js
@@ -0,0 +1,13 @@
+//level 5
+console.log("LEVEL 5");
+
+const firstName = "Welly"
+const lastName = "Andriani"
+
+const resultC = firstName + " " + lastName
+const resultD = `${firstName} ${lastName}`
+const resultE = firstName + lastName
+
+console.log(resultC);
+console.log(resultD);
+console.log(resultE);
