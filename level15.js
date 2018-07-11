9e39650536d399faaa47eddabe1aadd4c021115c
wellyandriani <wellyandriani97@gmail.com>
2018-07-11 11:29:15 +0700

    Add level 15

---
+++ level15.js
@@ -0,0 +1,13 @@
+// level 15
+console.log("LEVEL 15")
+
+var add = Math.round(2,6) + Math.pow(8,3);
+var substract = Math.PI - Math.round(4,2);
+var multiply = Math.pow(3,2) * Math.PI;
+var divide = Math.pow(6,3) / Math.PI;
+
+console.log(add);
+console.log(substract);
+console.log(multiply);
+console.log(divide);
+ 
