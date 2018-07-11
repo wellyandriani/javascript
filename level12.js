99f3015dc4338d970c86000e7b7c2b1fb1bd701a
wellyandriani <wellyandriani97@gmail.com>
2018-07-11 10:07:17 +0700

    add level 12

---
+++ level12.js
@@ -0,0 +1,10 @@
+//level 12
+console.log("LEVEL 12");
+
+function process(parameterA, parameterB){
+    console.log(parameterA * parameterB)
+    console.log(parameterA - parameterB)
+}
+
+process('3','9');
+process('10','15');
