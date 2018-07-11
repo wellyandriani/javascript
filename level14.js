4d047708d8fc7e905c9b5978c5b7fc2a132b9a68
wellyandriani <wellyandriani97@gmail.com>
2018-07-11 11:04:28 +0700

    Add level 14 math operations

---
+++ level14.js
@@ -0,0 +1,10 @@
+// level 14
+console.log("LEVEL 14")
+
+var resulPi = Math.PI;
+var resultround = Math.round(5.7);
+var resultpow = Math.pow(9,3);
+
+console.log(resulPi);
+console.log(resultpow);
+console.log(resultround);
