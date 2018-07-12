19455052c599d22dedb5be248df991591b284582
wellyandriani <wellyandriani97@gmail.com>
2018-07-12 16:43:22 +0700

    add level 7

---
+++ project1/level17.js
@@ -0,0 +1,10 @@
+//level17
+console.log("Level 17");
+
+var p = prompt ("Masukkan nilai panjang");
+p = parseInt (p);
+var l =prompt("masukkan nilai lebar");
+l = parseInt(l);
+var luas ;
+luas = p * l;
+document.write("Luas persegi panjang adalah: " + luas);
