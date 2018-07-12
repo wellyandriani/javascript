e975e7c47ea474a7e0d090028757f387f6ab7d5b
wellyandriani <wellyandriani97@gmail.com>
2018-07-12 15:05:30 +0700

    add level 5 Modify names and remove non String array value

---
+++ project2/level5.js
@@ -0,0 +1,30 @@
+//level 5
+console.log("LEVEL 5");
+
+const namee = [
+    "Mr. Fahri",
+    null,
+    undefined,
+    "Mr. Abdul",
+    24,
+    "Mrs. Josephhine",
+    "Mr. Joseph",
+    "Mr. Paul",
+    32,
+    "Mrs. Paula",
+    7,
+    22,
+    "Mr. Hakim"
+  ];
+
+  function remove(array){
+      array.forEach(element => {
+          if(typeof element === 'number') {
+              console.log(element)
+          }
+          
+      });
+  }
+
+remove(namee)
+  
