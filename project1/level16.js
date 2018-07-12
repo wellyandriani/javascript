9b6e5a6480923de1a370178cdbee1e066a483d23
wellyandriani <wellyandriani97@gmail.com>
2018-07-12 16:29:10 +0700

    edit level 16

--- project1/level16.js
+++ project1/level16.js
@@ -1,8 +1,5 @@
 // level 16
 console.log("LEVEL 16")
- 
-var bb = 9;
-var cc = 8;
 
     function add(bb, cc){
         return bb + cc;
@@ -20,10 +17,4 @@ var cc = 8;
       return bb % cc;
   }
 
-  add(substract(2,1), divide(4,3))
-
-console.log(add(bb, cc));
-console.log(subtract(bb, cc));
-console.log(multiply(bb, cc));
-console.log(divide(bb, cc));
-console.log(modulo(bb, cc));
+  console.log(add(subtract(2, 1), divide(4, 3)));
