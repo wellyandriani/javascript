f8803ae94f20f353b834fd306fd8508813faeb28
wellyandriani <wellyandriani97@gmail.com>
2018-07-12 14:20:21 +0700

    Add Level 1 Display all names with Mr. title

--- project2/level1.js
+++ project2/level1.js
@@ -16,3 +16,21 @@ const name = [
 name.forEach(list=>{
     console.log(list)
 });
+
+const displayMisterTren = function (array){
+    const filtered = array.filter(function (item){
+        return item.charAt(2) !== "s"
+    })
+}
+
+const displayMister = (array) => {
+    const filtered = array.filter(item => item.charAt(2)!== "s")
+    console.log(filtered);
+}
+const displayMistress = (array) => {
+    const filtered = array.filter(item => item.charAt(2) === "s")
+    console.log(filtered);
+}
+
+displayMister(name)
+displayMistress(name)
